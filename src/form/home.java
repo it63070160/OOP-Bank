5c2515167ab5a4b50ee2e684e067929e0b919d02
OHMpwpp <71824235+ohmzasa203@users.noreply.github.com>
2021-12-10 20:18:21 +0700

    Add files via upload

---
+++ src/form/home.java
@@ -0,0 +1,150 @@
+
+package form;
+
+import Model.Model_Card;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import swing.ScrollBar;
+
+public class home extends javax.swing.JPanel {
+
+
+    public home() {
+        initComponents();
+        
+        //set card ด้วย [card1-3].setData(new Model_Card("ชื่อบัญชี", "จำนวนเงิน", "เลขบัญชี"));
+        card1.setData(new Model_Card("Kim", "$200", "63070164"));
+        //card1.setData(new Model_Card("AccountName", "Balance", "AccountNum"));
+        //card1.setData(new Model_Card("AccountName", "Balance", "AccountNum"));
+        
+        jScrollPane1.setVerticalScrollBar(new ScrollBar());
+        jScrollPane1.getVerticalScrollBar().setBackground(Color.WHITE);
+        JPanel p = new JPanel();
+        p.setBackground(Color.WHITE);
+        jScrollPane1.setCorner(JScrollPane.UPPER_RIGHT_CORNER, p);
+        jScrollPane1.getViewport().setBackground(Color.WHITE);
+        
+        //เพิ่ม transaction ด้วย table.addRow(new Object[]{"ชื่อบัญชี", "ประเภทธุรกรรม (Deposit, Withdrawn, Transfer)", "วันที่ เวลา", "จำนวนเงิน"});
+        table.addRow(new Object[]{"AccoutName ชื่อบัญชี", "Deposit", "12/10/2021-14:38", "200"});
+        table.addRow(new Object[]{"AccoutName", "Deposit", "12/10/2021-14:38", "200"});
+        table.addRow(new Object[]{"AccoutName", "Deposit", "12/10/2021-14:38", "200"});
+        table.addRow(new Object[]{"AccoutName", "Deposit", "12/10/2021-14:38", "200"});
+        table.addRow(new Object[]{"AccoutName", "Deposit", "12/10/2021-14:38", "200"});
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panel = new javax.swing.JLayeredPane();
+        card1 = new component.card();
+        card2 = new component.card();
+        card3 = new component.card();
+        panel1 = new swing.panel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new swing.Table();
+
+        setBackground(new java.awt.Color(242, 242, 242));
+        setPreferredSize(new java.awt.Dimension(1200, 710));
+
+        panel.setLayout(new java.awt.GridLayout(1, 0, 10, 0));
+
+        card1.setColor1(new java.awt.Color(51, 51, 153));
+        card1.setColor2(new java.awt.Color(255, 0, 204));
+        panel.add(card1);
+
+        card2.setColor1(new java.awt.Color(58, 97, 134));
+        card2.setColor2(new java.awt.Color(137, 37, 62));
+        panel.add(card2);
+
+        card3.setColor1(new java.awt.Color(44, 62, 80));
+        card3.setColor2(new java.awt.Color(76, 161, 175));
+        panel.add(card3);
+
+        panel1.setBackground(new java.awt.Color(255, 255, 255));
+        panel1.setPreferredSize(new java.awt.Dimension(1030, 485));
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 26)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(98, 98, 98));
+        jLabel1.setText("Transactions");
+
+        jScrollPane1.setBorder(null);
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Account", "Transaction", "Date", "Amount"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        table.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
+        jScrollPane1.setViewportView(table);
+
+        javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
+        panel1.setLayout(panel1Layout);
+        panel1Layout.setHorizontalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1123, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20))
+        );
+        panel1Layout.setVerticalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 381, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1163, Short.MAX_VALUE))
+                .addGap(20, 20, 20))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private component.card card1;
+    private component.card card2;
+    private component.card card3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLayeredPane panel;
+    private swing.panel panel1;
+    private swing.Table table;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/main/frame.java
@@ -0,0 +1,173 @@
+
+package main;
+
+
+import event.*;
+import form.*;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class frame extends javax.swing.JFrame implements ActionListener {
+    ImageIcon winlogo = new ImageIcon(getClass().getClassLoader().getResource("minorcomponent/winlogo.png"));
+
+    public frame() {
+        initComponents();
+        setBackground(new Color(0, 0, 0, 0));
+        menu1.initMoving(frame.this);
+        setIconImage(winlogo.getImage());
+        header1.getMin().addActionListener(this);
+        menu1.addEventMenuSelected(new EventMenuSelected(){
+           @Override
+           public void selected(int index){
+               System.out.println(index);
+               if(index==0){
+                   setForm(new home());
+               }
+               else if(index==1){
+                   
+               }
+               else if(index==2){
+                   
+               }
+               else if(index==3){
+                   
+               }
+               else if(index==4){
+                   
+               }
+               else if(index==5){
+                   
+               }
+               else if(index==6){
+                   
+               }
+               else if(index==7){
+                   
+               }
+               else if(index==8){
+                   
+               }
+               else if(index==9){
+                   
+               }
+               else if(index==10){
+                   
+               }
+           }
+        });
+        setForm(new home());
+    }
+    
+    private void setForm(JComponent com){
+//        main_panel.removeAll();
+//        main_panel.add(com);
+//        main_panel.repaint();
+//        main_panel.revalidate();
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        setState(ICONIFIED);
+    }
+    
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panel1 = new swing.panel();
+        header1 = new component.Header();
+        menu1 = new component.menu();
+        sp = new javax.swing.JScrollPane();
+        home2 = new form.home();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(1600, 820));
+
+        panel1.setBackground(new java.awt.Color(242, 242, 242));
+
+        menu1.setPreferredSize(new java.awt.Dimension(250, 660));
+
+        sp.setBorder(null);
+        sp.setViewportView(home2);
+
+        javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
+        panel1.setLayout(panel1Layout);
+        panel1Layout.setHorizontalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(sp, javax.swing.GroupLayout.DEFAULT_SIZE, 1259, Short.MAX_VALUE)
+                    .addComponent(header1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, 0))
+        );
+        panel1Layout.setVerticalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addComponent(header1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(sp, javax.swing.GroupLayout.DEFAULT_SIZE, 770, Short.MAX_VALUE))
+            .addComponent(menu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new frame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private component.Header header1;
+    private form.home home2;
+    private component.menu menu1;
+    private swing.panel panel1;
+    private javax.swing.JScrollPane sp;
+    // End of variables declaration//GEN-END:variables
+}
